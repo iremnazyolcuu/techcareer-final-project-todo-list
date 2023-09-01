22b83315f2ca68bdc9dea95437e96e1b9eb7fcd3
iremnazyolcuu <iremnazyolcu@gmail.com>
2023-09-01 22:36:00 +0300

    assist

---
+++ src/main/java/com/iremnazyolcu/assist/PostmanPersist.java
@@ -0,0 +1,49 @@
+package com.iremnazyolcu.assist;
+
+public class PostmanPersist {
+
+    // Project URL: http://localhost:4444/
+    // H2Db URL: http://localhost:4444/h2-console/
+    // Swagger URL: http://localhost:4444/swagger-ui/index.html
+
+    // POSTMAN :
+    // CREATE
+    // http://localhost:4444/api/todos/create
+    /*
+    {
+        "title" : "demo title techcrr",
+        "description" : "demo description techcrr",
+        "completed" : true
+    }
+     */
+
+    // GET
+    // http://localhost:4444/api/todos/list/1
+
+    // GET ALL
+    // http://localhost:4444/api/todos/list/all
+
+    // UPDATE
+    // http://localhost:4444/api/todos/update/1
+
+    // DELETE
+    // http://localhost:4444/api/todos/delete/1
+
+    // DELETE ALL
+    // http://localhost:4444/api/todos/delete/all
+
+    // COMPLETE
+    // http://localhost:4444/api/todos/complete/1
+
+    // INCOMPLETE
+    // http://localhost:4444/api/todos/incomplete/1
+
+    // GET COMPLETED
+    // http://localhost:4444/api/todos/completed/true
+
+    // DELETE COMPLETED
+    // http://localhost:4444/api/todos/delete/completed
+
+}
+
+
